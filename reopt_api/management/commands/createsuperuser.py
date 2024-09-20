02f1360b4c4650c174b2296f10b1205e3f86f12e
Orr Mendelson <orrmendelson@gmail.com>
2024-09-20 11:13:08 +0300

    Add createsuperuser. Not tested. Still Admin without styling.

---
+++ reopt_api/management/commands/createsuperuser.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Create a superuser'
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='a@a.com',
+                password='admin'
+            )
+            self.stdout.write(self.style.SUCCESS('Successfully created superuser'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
